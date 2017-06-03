057593190a5dfef2d2a8c9f3820a75442d96b71d
sergiodiezdepedro <sergio.d.depedro@gmail.com>
2017-06-03 13:53:29 +0200

    Funcionalidades básicas del CRUD operativas v7

---
+++ public/js/main.js
@@ -0,0 +1,19 @@
+/* jshint esversion: 6 */
+
+$(document).ready(function () {
+    $('.delete-article').on('click', function (e) {
+        $target = $(e.target);
+        const id = $target.attr('data-id');
+        $.ajax({
+            type: 'DELETE',
+            url: '/article/'+id,
+            success: function (response) {
+                alert('Deleting Article');
+                window.location.href = '/';
+            },
+            error: function (err) {
+                console.log(err);
+            }
+        });
+    });
+});
